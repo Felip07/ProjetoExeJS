e0a35d15db558a61bdaad87c5ffb01bbb5525654
Felipe Souza <felipe255255255@gmail.com>
2023-11-21 22:40:14 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const containerGridSize = document.getElementById("container");
 const conteudoDiv = document.getElementById("conteudo-div");
+const exercicioAtual = document.getElementById("exercicios");
 
 function chamarExercicio(exercicio) {
   containerGridSize.style.gridTemplateRows = "10vh 80vh 80vh 10vh";
-  conteudoDiv.className = "conteudo-div";
+  // conteudoDiv.className = "conteudo-div";
 
   if (exercicio === "exercicio1") {
+    conteudoDiv.classList.remove("conteudo-div");
+    conteudoDiv.classList.add("ativo");
+    exercicioAtual.classList.add("ativo");
     conteudoDiv.scrollIntoView({
       behavior: "smooth",
     });
-    exercicio1();
+    // conteudoDiv.innerHTML = `<h2 class='title'>Calculadora</>`
   } else if (exercicio === "exercicio2") {
     conteudoDiv.scrollIntoView({
       behavior: "smooth",
@@ -31,34 +35,6 @@ function chamarExercicio(exercicio) {
     });
     conteudoDiv.innerHTML = `<h2 class='title'>Contador</>`;
   }
-}
-
-function exercicio1() {
-  conteudoDiv.innerHTML = `<h2 class='title'>Conversão de Moedas</h2>
-          <input type="number" placeholder="Insira o valor..." class="check-placeholder" id="value">
-          <br>Converter para:
-          <input type="radio" name="conversao" id="01">EUR
-          <input type="radio" name="conversao" id="02">USD
-          <br><input type="button" value="Converter" class="button-type">
-          <p id="result"></p>
-          `;
-
-  let value = Number(document.getElementById("value").value)
-  
-  const EURChecked = document.getElementById("01")
-  const USDChecked = document.getElementById("02")
-
-  if (EURChecked) {
-    value = value * 0.19
-    var resultado = document.getElementById("result")
-  } else if (USDChecked) {
-    value = value * 0.20
-    var resultado = document.getElementById("result")
-  } else {
-    var resultado = document.getElementById("result")
-    resultado.textContent = 'Nenhum valor inserido!'
-  }
-  resultado.textContent = `${value}`
 }
 
 function reload() {
